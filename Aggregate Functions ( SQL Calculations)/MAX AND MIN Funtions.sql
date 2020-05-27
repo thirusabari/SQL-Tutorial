c37de944a5b3ed206b3170367980f7f78a172687
Thirusabari <gtarasu1993@gmail.com>
2020-05-27 23:04:57 +0530

    Max and Min, Sum Function
    Max and Min, Sum Function

---
+++ Aggregate Functions ( SQL Calculations)/MAX AND MIN Funtions.sql
@@ -0,0 +1,11 @@
+-- 'MAX()' Function
+-- 'MIN()' Function
+-- MAX() And MIN() return the highest and lowest values in a column.
+
+SELECT MAX(ID) AS MaxID FROM Aggregates;
+
+SELECT MIN(ID) AS MinID FROM Aggregates;
+
+
+
+
---
+++ Aggregate Functions ( SQL Calculations)/SUM FUNCTION.sql
@@ -0,0 +1,3 @@
+-- 'SUM()' Function
+-- It takes the name of a column as an argument and returns the sum of all values in that column.
+SELECT SUM(ID) AS TotalIDs FROM Aggregates;
